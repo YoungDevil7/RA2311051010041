ea4c90169f835b211c2aa9df31b88665fb2cd82d
Sourya Varma <datlasouryavarma@gmail.com>
2026-05-02 11:05:52 +0530

    Add Bearer token authentication to logger and scheduler

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const LOG_API = "http://20.207.122.201/evaluation-service/logs";
+const BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzZDQ3NTVAc3JtaXN0LmVkdS5pbiIsImV4cCI6MTc3NzY5OTYzOSwiaWF0IjoxNzc3Njk4NzM5LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZmU5NzdjNzAtMmEyMS00YjQxLTkyOWMtYTJlMzAxZDA2MDIxIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic291cnlhIHZhcm1hIGRhdGxhIiwic3ViIjoiNGIwY2FhYWItYjlmZS00ZDQwLThkYzktMTIxZTIxMmM0ZTJlIn0sImVtYWlsIjoic2Q0NzU1QHNybWlzdC5lZHUuaW4iLCJuYW1lIjoic291cnlhIHZhcm1hIGRhdGxhIiwicm9sbE5vIjoicmEyMzExMDUxMDEwMDQxIiwiYWNjZXNzQ29kZSI6IlFrYnB4SCIsImNsaWVudElEIjoiNGIwY2FhYWItYjlmZS00ZDQwLThkYzktMTIxZTIxMmM0ZTJlIiwiY2xpZW50U2VjcmV0IjoiWFhjakFlSHZnRE5idUtVTiJ9.JFevg_bHUVU1dNhKNvVhNSn9gQghqttSWgS2ioTVxso";
 
 async function Log(stack, level, package, message) {
     try {
@@ -13,7 +14,8 @@ async function Log(stack, level, package, message) {
 
         const response = await axios.post(LOG_API, payload, {
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${BEARER_TOKEN}`
             }
         });
 
--- vehicle_maintenance_scheduler/scheduler.js
+++ vehicle_maintenance_scheduler/scheduler.js
@@ -2,20 +2,24 @@ const axios = require('axios');
 const Log = require('../logging_middleware/logger');
 
 const BASE_URL = "http://20.207.122.201/evaluation-service";
+const BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzZDQ3NTVAc3JtaXN0LmVkdS5pbiIsImV4cCI6MTc3NzY5OTYzOSwiaWF0IjoxNzc3Njk4NzM5LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZmU5NzdjNzAtMmEyMS00YjQxLTkyOWMtYTJlMzAxZDA2MDIxIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic291cnlhIHZhcm1hIGRhdGxhIiwic3ViIjoiNGIwY2FhYWItYjlmZS00ZDQwLThkYzktMTIxZTIxMmM0ZTJlIn0sImVtYWlsIjoic2Q0NzU1QHNybWlzdC5lZHUuaW4iLCJuYW1lIjoic291cnlhIHZhcm1hIGRhdGxhIiwicm9sbE5vIjoicmEyMzExMDUxMDEwMDQxIiwiYWNjZXNzQ29kZSI6IlFrYnB4SCIsImNsaWVudElEIjoiNGIwY2FhYWItYjlmZS00ZDQwLThkYzktMTIxZTIxMmM0ZTJlIiwiY2xpZW50U2VjcmV0IjoiWFhjakFlSHZnRE5idUtVTiJ9.JFevg_bHUVU1dNhKNvVhNSn9gQghqttSWgS2ioTVxso";
 
 async function getDepots() {
     await Log("backend", "info", "scheduler", "Fetching depots");
-    const res = await axios.get(`${BASE_URL}/depots`);
+    const res = await axios.get(`${BASE_URL}/depots`, {
+        headers: { 'Authorization': `Bearer ${BEARER_TOKEN}` }
+    });
     return res.data.depots;
 }
 
 async function getVehicles(depotId) {
     await Log("backend", "info", "scheduler", `Fetching vehicles for depot ${depotId}`);
-    const res = await axios.get(`${BASE_URL}/vehicles?depotId=${depotId}`);
+    const res = await axios.get(`${BASE_URL}/vehicles?depotId=${depotId}`, {
+        headers: { 'Authorization': `Bearer ${BEARER_TOKEN}` }
+    });
     return res.data.vehicles;
 }
 
-// 0/1 Knapsack to maximize Impact
 function knapsack(vehicles, capacity) {
     const n = vehicles.length;
     const dp = Array(n + 1).fill().map(() => Array(capacity + 1).fill(0));
